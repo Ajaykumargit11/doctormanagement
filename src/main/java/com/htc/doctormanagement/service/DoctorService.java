bb6f50018f0f1dfcb0e603fd6179e4c9eacd603a
muniirds <muniirds@gmail.com>
2021-09-09 08:44:50 +0530

    First Commit

---
+++ src/main/java/com/htc/doctormanagement/service/DoctorService.java
@@ -0,0 +1,69 @@
+package com.htc.doctormanagement.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+
+import com.htc.doctormanagement.dao.AppointmentInterface;
+import com.htc.doctormanagement.dao.DoctorInterface;
+import com.htc.doctormanagement.entity.Doctor;
+
+
+@Service
+public class DoctorService {
+	        @Autowired
+			private DoctorInterface doctorInterface;
+			@Autowired
+			private AppointmentInterface appointmentInterface;
+
+			@Transactional
+	public ResponseEntity<Object> createDoctordetails(Doctor doctor)
+	{
+		Doctor newDoctor = new Doctor();
+		
+		        newDoctor.setDoctorId(doctor.getDoctorId());
+		
+		        newDoctor.setDoctorName(doctor.getDoctorName());
+		
+		        newDoctor.setSpecialization(doctor.getSpecialization());
+		        newDoctor.setAppointment(doctor.getAppointment());
+		
+		        Doctor savedDoctor = doctorInterface.save(newDoctor);
+				return ResponseEntity.accepted().body(savedDoctor);
+		       
+		
+	}
+
+	
+	
+	  public List<Doctor> getAllDoctordetails() {
+	   
+	    List<Doctor> list = new ArrayList<>();
+	    Iterable<Doctor> customers = doctorInterface.findAll();
+	 
+	    customers.forEach(list::add);
+	    return list;
+	  }
+	 
+	
+
+	Doctor getDoctorbyId(Long doctorId)
+	{
+		Doctor doctor=null;
+		doctor=doctorInterface.findById(doctorId).get();
+		return doctor;
+		
+	}
+		public Doctor findByspecialization( String specialization) {
+
+			return doctorInterface.findByDoctorspecialization(specialization);
+		}
+	
+	
+}
